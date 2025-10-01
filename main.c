fe61d10c2776788245f82e7e44dc57b44470e51a
mikhan253 <miklautsch25@gmail.com>
2025-10-01 10:58:23 +0200

    test cyclic

--- main.c
+++ main.c
@@ -6,7 +6,17 @@
 #include <errno.h>
 
 void cyclic_task() {
-    printf("Cyclic task executed\n");
+    static uint64_t last_ms = 0;
+    uint64_t now_ms = 0;
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    now_ms = ts.tv_sec * 1000 + ts.tv_nsec / 1000000;
+    if (last_ms != 0) {
+        printf("Cyclic task executed, delta: %llu ms\n", (unsigned long long)(now_ms - last_ms));
+    } else {
+        printf("Cyclic task executed, first call\n");
+    }
+    last_ms = now_ms;
 }
 
 int main() {
